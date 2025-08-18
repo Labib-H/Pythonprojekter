e5b61e69e595951dd28f5b30b22abac658325f47
Labib <next16267@edu.nextkbh.dk>
2025-08-18 09:34:01 +0200

    dupe

---
+++ typecasting.py
@@ -0,0 +1,19 @@
+
+menu = "potato"
+
+print(f"what is on the menu? We got {menu}")
+
+score = 9000
+
+print(f"you need atleast {score} trophies to join the clan")
+
+average_elixir = 3.4
+
+print(f"my average elixir is {average_elixir} which is low")
+
+clan_member = False
+
+if clan_member: 
+    print("you are a clan member")
+else:
+    print(f"you need {score} trophies to join the clan")
