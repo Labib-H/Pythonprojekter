97f6868dfd16621ce2054a0bc197e73b1483df13
Labib <next16267@edu.nextkbh.dk>
2025-08-20 08:31:25 +0200

    Ændring af vairabler

--- typecasting.py
+++ typecasting.py
@@ -1,1 +1,14 @@
-#typecasting
+#typecasting - ændre mellem forskellige typer af variabler
+
+clan = ""
+medlemmer = 23
+avelixir = 5.5
+medlem = True
+
+medlemmer = str(medlemmer)
+avelixir = int(avelixir)
+clan = bool(clan)
+print(type(medlemmer))
+print(type(avelixir))
+print(clan)
+
